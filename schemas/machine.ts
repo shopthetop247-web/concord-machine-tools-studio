6931aff762648942973cebae62afe1bdf51d9451
shopthetop247-web <shopthetop247@gmail.com>
2026-01-05 16:12:39 -0500

    Update machine.ts

--- schemas/machine.ts
+++ schemas/machine.ts
@@ -9,42 +9,48 @@ export default {
       name: 'name',
       title: 'Name',
       type: 'string',
-      validation: (Rule: any) => Rule.required()
+      validation: Rule => Rule.required()
     },
     {
       name: 'stockNumber',
       title: 'Stock #',
       type: 'string',
-      validation: (Rule: any) => Rule.required()
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'yearOfMfg',
+      title: 'Year of Mfg.',
+      type: 'number',
+      validation: Rule => Rule.required()
+        .min(1900)
+        .max(new Date().getFullYear())
     },
     {
       name: 'category',
       title: 'Category',
       type: 'reference',
       to: [{ type: 'category' }],
-      validation: (Rule: any) => Rule.required()
+      validation: Rule => Rule.required()
     },
     {
       name: 'subcategory',
       title: 'Subcategory',
       type: 'reference',
       to: [{ type: 'subcategory' }],
-      validation: (Rule: any) => Rule.required()
+      validation: Rule => Rule.required()
     },
     {
-      name: 'price',
-      title: 'Price',
-      type: 'number'
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: { hotspot: true } // allows cropping/focusing
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image'
-    },
-    {
-      name: 'description',
-      title: 'Description',
+      name: 'specifications',
+      title: 'Specifications',
       type: 'text'
     }
+    // Price field removed
   ]
 }
