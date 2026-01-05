97362d65ac9378e0f66229d79afde7167b1fb261
shopthetop247-web <shopthetop247@gmail.com>
2026-01-05 13:07:00 -0500

    Create index.ts

---
+++ schemas/index.ts
@@ -0,0 +1,9 @@
+import category from './category'
+import subcategory from './subcategory'
+import machine from './machine'
+
+export const schemaTypes = [
+  category,
+  subcategory,
+  machine
+]
