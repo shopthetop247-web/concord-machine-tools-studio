bbc83bb62f53aaa0813cf31418c02ee95adca786
shopthetop247-web <shopthetop247@gmail.com>
2026-01-05 13:06:24 -0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@ import { schemaTypes } from './schemas'
 export default defineConfig({
   name: 'concord-machine-tools',
   title: 'Concord Machine Tools',
-  projectId: '<YOUR_PROJECT_ID>', // Replace with your actual Sanity project ID
+  projectId: 'iwodjd3n', // Replace with your actual Sanity project ID
   dataset: 'production',
   basePath: '/studio',
   schema: {
