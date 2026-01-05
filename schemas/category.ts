c27bd2526bae099be803608fd23f01ed677680bd
shopthetop247-web <shopthetop247@gmail.com>
2026-01-05 13:07:24 -0500

    Create category.ts

---
+++ schemas/category.ts
@@ -0,0 +1,22 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Category Name',
+      type: 'string',
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title', maxLength: 96 },
+      validation: Rule => Rule.required()
+    })
+  ]
+})
