c3922a9417f838126675899f2bf6a915d1a1dee9
shopthetop247-web <shopthetop247@gmail.com>
2026-02-05 13:41:45 -0500

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,7 +1,8 @@
 import category from './category'
 import subcategory from './subcategory'
 import machine from './machine'
-import brand from './brand';
+import brand from './brand'
+import post from './post';
 
 export const schemaTypes = [
   category,
