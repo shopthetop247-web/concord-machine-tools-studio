24036bdd338a6e2450d808b96fc9715015c6b720
shopthetop247-web <shopthetop247@gmail.com>
2026-02-07 08:04:15 -0500

    Create tag.ts

---
+++ schemas/tag.ts
@@ -0,0 +1,24 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'tag',
+  title: 'Tag',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Tag',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+})
